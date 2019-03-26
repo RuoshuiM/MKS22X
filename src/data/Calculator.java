1449cf741505ec99786f9f854b686c991504a571
Ruoshui <rmao10@stuy.edu>
2019-03-26 18:14:51 -0400

    Implements mod op

--- src/data/Calculator.java
+++ src/data/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
      */
     public static double eval(String s) {
         String[] elements = s.split(" ");
-        
+
         MyDeque<Double> stack = new MyDeque<>();
         for (String e : elements) {
             if (e.matches("([0-9]*.)?[0-9]+")) {
@@ -25,6 +25,10 @@ public class Calculator {
                     double b = stack.pop();
                     double a = stack.pop();
                     stack.push(a / b);
+                } else if (e.equals("%")) {
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(a % b);
                 } else {
                     throw new IllegalArgumentException("Invalid token: " + e);
                 }
@@ -32,7 +36,7 @@ public class Calculator {
         }
         return stack.pop();
     }
-    
+
     public static void main(String...args) {
         String[] cases = {"10 2.0 +", "11 3 - 4 + 2.5 *", "8 2 + 99 9 - * 2 + 9 -", "1 2 3 4 5 + * - -"};
         double[] expected = {12.0, 30.0, 893.0, 26.0};
@@ -40,7 +44,7 @@ public class Calculator {
             test(cases[i], expected[i]);
         }
     }
-    
+
     private static void test(String s, double expected) {
         System.out.format("Expected: %f, got: %f%n", expected, eval(s));
     }
